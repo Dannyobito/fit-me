075863da6b798aae0508053cd9ce23ee6b5016f8
Dannyobito <dannyobito05@gmail.com>
2023-10-24 21:19:20 +0100

    added cart component, cart dropdown component and cart action and reducer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import Logo from './logo.png';
+import CartIcon from '../cart-icon/CartIcon';
+import CartDropdown from '../cart-dropdown/CartDropdown';
 import './Header.scss';
 import { Link } from 'react-router-dom';
 const Header = ({currentUser}) => {
@@ -18,7 +20,9 @@ const Header = ({currentUser}) => {
             <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>
             : <Link className='option' to={'/signin-signup'}>SIGN IN</Link>
           }
+          <CartIcon />
         </div>
+        <CartDropdown />
     </div>
   )
 }
